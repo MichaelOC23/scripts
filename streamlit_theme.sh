aaafdfadc61ab93710e6c20e5a0a7e952b80ed56
Michael Smith <michaelsmith9905@gmail.com>
2024-10-17 10:24:29 -0700

    default commit message

---
+++ streamlit_theme.sh
@@ -0,0 +1,40 @@
+#! # DARK MODE
+export STREAMLIT_THEME_BASE="Custom-Dark"						# Uset this to set a dark or light standard theme
+export STREAMLIT_THEME_FONT="sans serif"                  # Font
+export STREAMLIT_THEME_PRIMARYCOLOR="#98CCD0"             # Accent
+export STREAMLIT_THEME_BACKGROUNDCOLOR="#003366"          # Main Body Background
+export STREAMLIT_THEME_SECONDARYBACKGROUNDCOLOR="#404040" # Sidebar / form background
+export STREAMLIT_THEME_TEXTCOLOR="#CBD9DF"                # Text Color (should contrast well with both Background Color and Secondary Background Color
+export STREAMLIT_LOGO_URL="https://firebasestorage.googleapis.com/v0/b/toolsexplorationfirebase.appspot.com/o/assets%2Flogo-white-gray.svg?alt=media&token=acaa8687-e6ff-42a7-a693-34b34cceefd6"
+export STREAMLIT_ICON_URL="https://firebasestorage.googleapis.com/v0/b/toolsexplorationfirebase.appspot.com/o/assets%2Ficon-white-gray.svg?alt=media&token=5550bb55-23c1-4151-9f35-642a3083f1d0"
+export STREAMLIT_PAGE_ICON="https://firebasestorage.googleapis.com/v0/b/toolsexplorationfirebase.appspot.com/o/assets%2Fpage-icon.svg?alt=media&token=5ecd156a-0371-442f-938e-2f3a3510b344"
+
+#! LIGHT MODE (COMMUNIFY_ISH)
+# export STREAMLIT_THEME_BASE="Custom-Light"						# Uset this to set a dark or light standard theme
+# export STREAMLIT_THEME_FONT="sans serif"                  # Font
+# export STREAMLIT_THEME_PRIMARYCOLOR="#FFFFFF"             # Accent
+# export STREAMLIT_THEME_BACKGROUNDCOLOR="#003366"          # Main Body Background
+# export STREAMLIT_THEME_SECONDARYBACKGROUNDCOLOR="#F3F8FF" # Sidebar / form background
+# export STREAMLIT_THEME_TEXTCOLOR="#1E1E1E"                # Text Color (should contrast well with both Background Color and Secondary Background Color
+# export STREAMLIT_LOGO_URL="https://firebasestorage.googleapis.com/v0/b/toolsexplorationfirebase.appspot.com/o/assets%2Flogo-black-gray.svg?alt=media&token=85e3cbba-ef8d-4d8b-9943-88f6b179e1c0"
+# export STREAMLIT_ICON_URL="https://firebasestorage.googleapis.com/v0/b/toolsexplorationfirebase.appspot.com/o/assets%2Ficon-black-gray.svg?alt=media&token=7ebaedc9-1c3f-45cb-b54f-28741b7067e3"
+# export STREAMLIT_PAGE_ICON="https://firebasestorage.googleapis.com/v0/b/toolsexplorationfirebase.appspot.com/o/assets%2Fpage-icon.svg?alt=media&token=5ecd156a-0371-442f-938e-2f3a3510b344"
+
+#Streamlit
+export STREAMLIT_BROWSER_SERVERADDRESS="localhost"
+export STREAMLIT_BROWSER_GATHERUSAGESTATS=false
+export STREAMLIT_SERVER_FOLDERWATCHBLACKLIST="[]"
+export STREAMLIT_SERVER_HEADLESS=true
+export STREAMLIT_SERVER_RUNONSAVE=true
+export STREAMLIT_SERVER_PORT=5010
+export STREAMLIT_SERVER_ENABLECORS=false
+export STREAMLIT_SERVER_ENABLEXSRFPROTECTION=true
+export STREAMLIT_SERVER_MAX_UPLOAD_SIZE=10000
+export STREAMLIT_SERVER_MAXMESSAGESIZE=10000
+export STREAMLIT_SERVER_ENABLEWEBSOCKETCOMPRESSION=false
+export STREAMLIT_SERVER_ENABLESTATICSERVING=false
+
+export STREAMLIT_RUNNER_MAGICENABLED=false
+export STREAMLIT_RUNNER_FASTRERUNS=true
+export STREAMLIT_RUNNER_ENFORCESERIALIZABLESESSIONSTATE=false
+export STREAMLIT_RUNNER_ENUMCOERCION="nameOnly"
